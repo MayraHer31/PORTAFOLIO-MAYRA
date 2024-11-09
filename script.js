84ac145f56a9f041856064a0ea5e0bec53b540d9
mayraHer <mayraaparcana@gmail.com>
2024-11-08 19:14:12 -0500

    Actualización del diseño responsivo

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//Mostrar/Ocultar el Menú
+// Seleccionamos el botón de menú y el menú de navegación
+const menuToggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.navegacion__lista');
+
+// Evento para alternar el estado del menú (abrir/cerrar)
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('active'); // Alterna la clase 'active' para abrir o cerrar el menú
+});
+
+// Seleccionamos todos los enlaces dentro del menú
+const navLinks = document.querySelectorAll('.navegacion__elemento a');
+
+// Añadimos un evento de clic a cada enlace para cerrar el menú cuando se hace clic
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navList.classList.remove('active'); // Cierra el menú
+  });
+});
+
+
+//Activar/Desactivar Modo Nocturno
 const toggle = document.getElementById('toggle');
 const body = document.body;
 const themeIcon = document.getElementById('theme-icon');
@@ -17,7 +39,7 @@ toggle.addEventListener('change', () => {
     }
 });
 
-
+// Función 'Volver Arriba'
 // Mostrar el botón cuando el usuario se desplaza hacia abajo 20px desde la parte superior de la página
 window.onscroll = function() {
     mostrarBoton();
